c225b97dd08c896c84354b19345c20e63f7484a5
krish <krishsrivatsav@gmail.com>
2019-12-18 11:08:30 +0530

    initial commit

---
+++ src/app/forres/forres.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forres',
+  templateUrl: './forres.component.html',
+  styleUrls: ['./forres.component.css']
+})
+export class ForresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
